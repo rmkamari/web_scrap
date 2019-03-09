1e4c176de89bd8f15cccd4976bef1f15afe38ac0
rmkamari <rmkamari@gmail.com>
2019-03-09 17:38:48 +0330

    list-find

--- test.py
+++ test.py
@@ -1,33 +1,42 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+import time
 
-baseurl = "https://www.tabnak.ir/"
+
+# baseurl = "https://www.farsnews.com"
+# baseurl = "https://www.tabnak.ir"
+baseurl = 'https://www.mashreghnews.ir'
+
 
 xpaths = {'title1': '//*[@id="home"]/div[3]/div[1]/div/div/section/div[2]/div[2]/div/div/div[2]/h3/a',
            'kol': '//*[@id="home"]/div[3]/div[1]/div/div/section',
            'submitButton':   "//input[@name='login']"
          }
+
+# pageContent = requests.get(baseurl)
+# tree = html.fromstring(pageContent.content)
+# print(tree)
+# data = tree.xpath('//*[@id="home"]/div[3]/div[1]/div/div/aside[3]/section/div[3]/div[2]/section/div')
+# for i in data:
+#     print(i)
+# raw_html = tree.cssselect("div.div_class ul > li")
+# raw_html = tree.xpath('//*[@id="home"]/div[3]/div[1]/div/div/aside[3]/section/div[3]/div[2]/section/div')
+# for item in raw_html:
+#     print(item.text_content())
 driver = webdriver.Chrome()
 driver.get(baseurl)
-t1 = driver.find_element_by_xpath(xpaths['kol'])
-print(t1.get_attribute('text'))
-print(t1.get_attribute('href'))
+data = driver.find_element_by_xpath('//*[@id="box16"]/div')
+list_html = data.get_property("innerHTML")
+soup = BeautifulSoup(list_html, 'html.parser')
 
+links = soup.find_all("a")
 
+for l in links:
+    if baseurl in l:
+        print(l['href'])
+    else:
+        driver.get(baseurl + l['href'])
+        time.sleep(4)
+        print(baseurl + l['href'])
 driver.close()
-# #Clear Username TextBox if already allowed "Remember Me"
-# driver.find_element_by_xpath(xpaths['usernameTxtBox']).clear()
-#
-# #Write Username in Username TextBox
-# driver.find_element_by_xpath(xpaths['usernameTxtBox']).send_keys(username)
-#
-# #Clear Password TextBox if already allowed "Remember Me"
-# driver.find_element_by_xpath(xpaths['passwordTxtBox']).clear()
-#
-# #Write Password in password TextBox
-# driver.find_element_by_xpath(xpaths['passwordTxtBox']).send_keys(password)
-#
-# #Click Login button
-# driver.find_element_by_xpath(xpaths['submitButton']).click()
